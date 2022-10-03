f7797c3de7c5f0000129800e5a744b5ebc01b8a9
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-03 20:58:36 +0200

    make the api client method more robust

--- NetworkingKit/Sources/NetworkingKit/Shared/APIClient.swift
+++ NetworkingKit/Sources/NetworkingKit/Shared/APIClient.swift
@@ -1,6 +1,6 @@
 //
 //  APIClient.swift
-//  
+//
 //
 //  Created by Abdeltawab Mohamed on 03/10/2022.
 //
@@ -8,24 +8,38 @@
 import Foundation
 
 protocol APIClient {
-    
     func sendRequest<T: Decodable>(
         endpoint: Endpoint,
         responseModel: T.Type
-    ) async throws -> T
+    ) async -> Result<T, RequestError>
 }
 
-//TODO: make more robust
+// TODO: make more robust
 
 extension APIClient {
-    
     func sendRequest<T: Decodable>(
         endpoint: Endpoint,
         responseModel: T.Type
-    ) async throws -> T {
+    ) async -> Result<T, RequestError> {
         var request = URLRequest(url: endpoint.url)
         request.httpMethod = endpoint.method.rawValue
-        let (data, _) = try await URLSession.shared.data(for: request, delegate: nil)
-        return try JSONDecoder().decode(T.self, from: data)
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+            guard let response = response as? HTTPURLResponse else {
+                return .failure(.unknown)
+            }
+            switch response.statusCode {
+            case 200 ... 299:
+                guard let model = try? JSONDecoder().decode(T.self, from: data) else {
+                    return .failure(.unknown)
+                }
+                return .success(model)
+            default:
+                return .failure(.unknown)
+            }
+
+        } catch {
+            return .failure(.unknown)
+        }
     }
 }
---
+++ NetworkingKit/Sources/NetworkingKit/Shared/RequestError.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdeltawab Mohamed on 03/10/2022.
+//
+
+enum RequestError: Error {
+    case unknown
+}
