115ea9a83e378fe1cbfbbeeba72fc7ce4769ab14
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-03 19:32:08 +0200

    add EndPoint class to handle endpoints creation

---
+++ NetworkingKit/Sources/NetworkingKit/Shared/EndPoint.swift
@@ -0,0 +1,46 @@
+//
+//  EndPoint.swift
+//  
+//
+//  Created by Abdeltawab Mohamed on 03/10/2022.
+//
+
+protocol Endpoint {
+    var defaultURL: URL { get}
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var url : URL { get}
+    var method: RequestMethod { get }
+    var body: [String: String]? { get }
+    var queryItems: [URLQueryItem] { get}
+
+}
+
+extension Endpoint {
+    var defaultURL: URL {
+       URL(string:"https://breakingbadapi.com/api")!
+    }
+    
+    var scheme: String {
+        "https"
+    }
+
+    var host: String {
+        "breakingbadapi.com"
+    }
+    
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components.url ?? defaultURL
+    }
+}
+
+enum RequestMethod: String {
+    case get = "GET"
+}
