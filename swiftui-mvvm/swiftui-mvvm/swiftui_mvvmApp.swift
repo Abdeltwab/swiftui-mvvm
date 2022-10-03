85d199d5777c2c90977f7928808bad03ef6f3b03
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-03 19:16:50 +0200

    create project

---
+++ swiftui-mvvm/swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Abdeltawab Mohamed on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
