9f45b5f6aa6fba2380824bb9a1a1cbfff14ed2f5
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-04 02:34:08 +0200

    finish characters list viewModel

--- swiftui-mvvm/swiftui-mvvm/Modules/CharacterList/Presentation/ViewModel/CharacterListViewModel.swift
+++ swiftui-mvvm/swiftui-mvvm/Modules/CharacterList/Presentation/ViewModel/CharacterListViewModel.swift
@@ -6,41 +6,59 @@
 //
 
 import Combine
+import Foundation
 
-class CharacterListViewModel: ObservableObject{
+class CharacterListViewModel: ObservableObject {
     @Published var filteredCharacters: [CharacterUIModel] = []
     @Published var searchText = ""
 
-    var test = ""
-    private var charactersResult: [CharacterUIModel] = []
+    @Published private var charactersList: [CharacterUIModel] = []
     private var cancellables: Set<AnyCancellable> = []
-    private let fetchCharactersUseCase : FetchCharacters
-    
-    
-    init(fetchCharacters:FetchCharacters) {
+    private let fetchCharactersUseCase: FetchCharacters
+
+    init(fetchCharacters: FetchCharacters) {
         fetchCharactersUseCase = fetchCharacters
+        subscribeToSearchTextChange()
         getCharactersList()
     }
 }
 
 private extension CharacterListViewModel {
     
-    func didSearchTextChange() {
-        
+    func subscribeToSearchTextChange() {
+        $searchText
+            .combineLatest($charactersList)
+            .debounce(for: .seconds(0.3),
+                      scheduler: DispatchQueue.main)
+            .map(filterCharactersList)
+            .sink { [weak self] characters in
+                guard let self = self else { return }
+                self.filteredCharacters = characters
+            }
+            .store(in: &cancellables)
     }
-    
+
+    func filterCharactersList(text: String,
+                              list: [CharacterUIModel]) -> [CharacterUIModel] {
+        text.isEmpty ?
+        list : list.filter {
+            $0.character.name.localizedCaseInsensitiveContains(text)
+        }
+    }
+
     func getCharactersList() {
         fetchCharactersUseCase
             .execute()
             .sink(receiveCompletion: { completion in
                 switch completion {
-                case .failure(let error):
+                case let .failure(error):
                     print(error)
                 case .finished:
                     print("finished")
                 }
             }, receiveValue: { [weak self] value in
-                self?.charactersResult = value
+                guard let self = self else { return }
+                self.charactersList = value
             }).store(in: &cancellables)
     }
 }
