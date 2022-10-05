8c35c8821265cbe79f0dd6f95bed3c2600454fd2
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-05 12:59:45 +0200

    add 2 unit testing

--- NetworkingKit/Tests/NetworkingKitTests/NetworkingKitTests.swift
+++ NetworkingKit/Tests/NetworkingKitTests/NetworkingKitTests.swift
@@ -1,11 +1,28 @@
 import XCTest
+import Combine
 @testable import NetworkingKit
 
 final class NetworkingKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NetworkingKit().text, "Hello, World!")
+    private var cancellables = Set<AnyCancellable>()
+
+    
+    func test_characters_list_parsing() throws {
+        let service = MockCharactersService()
+        let expectation = XCTestExpectation(description: "Publishes values then finishe")
+        var expectedList: [Character] = []
+        service.fetchCharacters()
+            .sink { completion in
+                guard case .finished = completion else { return }
+                expectation.fulfill()
+            } receiveValue: {
+                expectedList.append(contentsOf: $0)
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 0.5)
+        XCTAssertNotNil(expectedList.first)
+        XCTAssertEqual(expectedList.first!.id, 1)
+        XCTAssertEqual(expectedList.first!.name, "Walter White")
+        XCTAssertEqual(expectedList.first!.birthday, "09-07-1958")
     }
 }
--- swiftui-mvvm/swiftui-mvvmTests/CharatersListTests/CharatersListTests.swift
+++ swiftui-mvvm/swiftui-mvvmTests/CharatersListTests/CharatersListTests.swift
@@ -18,32 +18,12 @@ class CharatersListTests: XCTestCase {
     override func setUp()  {
         sut = CharacterListViewModel(fetchCharacters: FetchCharacters(service: MockCharactersService()))
     }
-
-    func test_LocalCharatersListIsValid() {
-//        let expectation = XCTestExpectation(description: "Publishes values then finishe")
-//        var expectedList: [CharacterUIModel] = []
-//    
-//        
-//        fetchCharacters.execute()
-//            .sink { completion in
-//                guard case .finished = completion else { return }
-//                expectation.fulfill()
-//            } receiveValue: {
-//                expectedList.append(contentsOf: $0)
-//            }
-//            .store(in: &cancellables)
-//        wait(for: [expectation], timeout: 0.5)
-//        XCTAssertEqual(expectedList.count, 5)
-//        XCTAssertNotNil(expectedList.first)
-//        XCTAssertEqual(expectedList.first!.id, 1)
-//        XCTAssertEqual(expectedList.first!.character.name, "Walter White")
-//        XCTAssertEqual(expectedList.first!.character.birthday, "09-07-1958")
+    
+    func test_fetch_charactersList() {
+        XCTAssertEqual(sut.charactersList.count, 5)
     }
     
-    func test_emptyFilterdList() {
-        sut.searchText = "jsess"
-        print("sut.charactersList.count")
-        print(sut.charactersList.count)
+    func test_fetch_charactersList() {
         XCTAssertEqual(sut.charactersList.count, 5)
     }
 
