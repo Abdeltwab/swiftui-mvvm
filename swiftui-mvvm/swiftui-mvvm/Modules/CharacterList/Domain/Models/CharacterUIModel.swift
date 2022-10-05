561a84465d81b92579c5190bfd4c957c6a005dad
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2022-10-05 14:21:52 +0200

    add defaultImageURL

--- swiftui-mvvm/swiftui-mvvm/Modules/CharacterList/Domain/Models/CharacterUIModel.swift
+++ swiftui-mvvm/swiftui-mvvm/Modules/CharacterList/Domain/Models/CharacterUIModel.swift
@@ -5,17 +5,21 @@
 //  Created by Abdeltawab Mohamed on 04/10/2022.
 //
 
-import NetworkingKit
 import Foundation
+import NetworkingKit
 
-struct CharacterUIModel: Identifiable, Equatable{
+struct CharacterUIModel: Identifiable, Equatable {
     let character: Character
-    
+
     var id: Int {
         character.id
     }
-    
-    var imageURL: URL{
-        URL(string: character.img) ?? URL(string: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg")!
+
+    var imageURL: URL {
+        URL(string: character.img) ?? defaultImageURL
+    }
+
+    private var defaultImageURL: URL {
+        URL(string: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg")!
     }
 }
